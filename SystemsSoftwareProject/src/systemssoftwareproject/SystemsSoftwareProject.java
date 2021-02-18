3a19406487a7de658ce87dfeed171d3d76f86f8c
JamieVoce <jamievoce0115@gmail.com>
2021-02-18 23:19:05 +0000

    login function added. Currently only requests a username and password

--- SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
@@ -12,25 +12,24 @@ public class SystemsSoftwareProject {
         System.out.println("Main Menu");
         boolean valid_option_chosen = false;
         while (!valid_option_chosen){
-            Scanner sc= new Scanner(System.in); // System.in is a standard input stream  
+            Scanner sc= new Scanner(System.in);
             System.out.print("Do you want to login or signup? ");   
             String menu_choice = sc.nextLine();  // reads string
             if(menu_choice.equals("login")){
-                System.out.println("U chose login");
+                login();
             }
             else if(menu_choice.equals("signup")){
-                System.out.println("U chose signup");
                 signup();
             }
             else {
-               System.out.println("That is not an option"); 
+               System.out.println("That is not a valid option"); 
             }
         }
     }
     
     public static void signup() {
-        boolean valid_signup_details = true;
-        while (valid_signup_details){
+        boolean valid_signup_details = false;
+        while (!valid_signup_details){
             Scanner sc= new Scanner(System.in); 
             System.out.print("Username: ");   
             String username = sc.nextLine();
@@ -41,6 +40,7 @@ public class SystemsSoftwareProject {
 
             if (username.equals(check_username)){
                 System.out.println("Usernames are equal");
+                valid_signup_details = true;
                 write_account_details(username, password);
                 /* At some point we can run some checks on the password.
                 I.e. does the password contain at least 1 alpha/num character.
@@ -66,6 +66,17 @@ public class SystemsSoftwareProject {
             pout.close(); // close the stream
         } catch (IOException e) {
             System.err.println("Error! - " + e.getMessage());
+        }
+    }
+    
+    public static void login() {
+        boolean logged_in = false;
+        while (!logged_in){
+            Scanner sc= new Scanner(System.in); 
+            System.out.print("Username: ");   
+            String login_username = sc.nextLine();
+            System.out.print("Password: "); 
+            String login_password = sc.nextLine();
         }
     }
 }
