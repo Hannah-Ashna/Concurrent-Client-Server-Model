d3ff936be9639a366efd26fe3c2ec5220036c0b8
JamieVoce <jamievoce0115@gmail.com>
2021-02-18 22:50:17 +0000

    Signup function added. Currently doesn't store account details however does ask for username, a second username that must equal the first and a password

--- SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
@@ -4,7 +4,7 @@ package systemssoftwareproject;
 import java.util.*; 
 
 public class SystemsSoftwareProject {
-
+    
     public static void main(String[] args) {
         System.out.println("Main Menu");
         boolean valid_option_chosen = false;
@@ -17,11 +17,35 @@ public class SystemsSoftwareProject {
             }
             else if(menu_choice.equals("signup")){
                 System.out.println("U chose signup");
+                signup();
             }
             else {
                System.out.println("That is not an option"); 
             }
         }
+    }
+    
+    public static void signup() {
+        boolean valid_signup_details = true;
+        while (valid_signup_details){
+            Scanner sc= new Scanner(System.in); 
+            System.out.print("Username: ");   
+            String username = sc.nextLine();
+            System.out.print("Confirm username: ");   
+            String check_username = sc.nextLine();
+            System.out.print("Password: "); 
+            String password = sc.nextLine();
 
+            if (username.equals(check_username)){
+                System.out.println("Usernames are equal");
+                /* At some point we can run some checks on the password.
+                I.e. does the password contain at least 1 alpha/num character.
+                */
+            }
+            else{
+                System.out.println("Usernames are not equal");
+                valid_signup_details = false;
+            }
+        }
     }
 }
