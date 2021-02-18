5f7424e79aa4be01be1608f080cb1d0a2ea1febe
JamieVoce <jamievoce0115@gmail.com>
2021-02-18 22:31:48 +0000

    Created a basic menu system that allows the user to choose if they want to login or signup

--- SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/SystemsSoftwareProject.java
@@ -1,9 +1,27 @@
 package systemssoftwareproject;
 
+// import java.io.*;
+import java.util.*; 
+
 public class SystemsSoftwareProject {
 
     public static void main(String[] args) {
-        System.out.println("Testing");
+        boolean valid_option_chosen = false;
+        while (!valid_option_chosen){
+            System.out.println("Main Menu");
+            Scanner sc= new Scanner(System.in); // System.in is a standard input stream  
+            System.out.print("Do you want to login or signup? ");   
+            String message = sc.nextLine();  // reads string
+            if("login".equals(message)){
+                System.out.println("U chose login");
+            }
+            else if("signup".equals(message)){
+                System.out.println("U chose signup");
+            }
+            else {
+               System.out.println("That is not an option"); 
+            }
+        }
+
     }
-    
 }
