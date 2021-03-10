52779fc0f897e3f1762b926fa1f6014344669a72
Ryan <n0799130@my.ntu.ac.uk>
2021-03-10 16:43:42 +0000

    Random temp generator

--- SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/WeatherInstruments.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/WeatherInstruments.java
@@ -6,9 +6,24 @@ import java.time.LocalDateTime;
 import systemssoftwareproject.DataStructures.SampleType;
 
 public class WeatherInstruments{
+
+    private double temp = 23.1; 
+
     public double getTemp(){
-        //will communicate with 'thermostat' and return the current temprature on request 
-        return 10.1;
+        //Communicate with thermostat and return on request the current temperature.
+            Random r = new Random();
+            if (temp > 35.0){
+                temp = 33.0;
+            }else{
+                //Get random number, decides if value increases or decreases.
+                int num = r.nextInt();
+                if (num < 0) {
+                        temp += 0.5;
+                } else {
+                        temp -= 0.5;
+                }
+            }
+            return temp;
     }
     public double getHumidity(){
         //Will request the humidity from the device and return the humidity
