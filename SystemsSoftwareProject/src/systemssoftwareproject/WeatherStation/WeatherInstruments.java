628e5d58349be9998622569ec42f6497aef8961d
Ryan <n0799130@my.ntu.ac.uk>
2021-03-13 16:34:33 +0000

    Update WeatherInstruments.java

--- SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/WeatherInstruments.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/WeatherInstruments.java
@@ -12,12 +12,12 @@ public class WeatherInstruments{
 
     public double getTemp(){
         //Communicate with thermostat and return on request the current temperature.
-            Random r = new Random();
+            Random t = new Random();
             if (temp > 35.0){
                 temp = 33.0;
             }else{
                 //Get random number, decides if value increases or decreases.
-                int num = r.nextInt();
+                int num = t.nextInt();
                 if (num < 0) {
                         temp += 0.5;
                 } else {
@@ -28,6 +28,19 @@ public class WeatherInstruments{
     }
     public double getHumidity(){
         //Will request the humidity from the device and return the humidity
+        double hum = (temp - 5);
+        Random h = new Random();
+            if (hum > 35.0){
+                hum = 33.0;
+            }else{
+                //Get random number, decides if value increases or decreases.
+                int val = h.nextInt();
+                if (val < 0) {
+                        hum += 4;
+                } else {
+                        hum -= 4;
+                }
+            }
         return 50.5;
     }
     public double getGpsLat(){
