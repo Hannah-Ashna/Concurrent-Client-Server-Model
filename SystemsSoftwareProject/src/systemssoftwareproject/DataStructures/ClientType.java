25074361ca485228850b6eec106d47a7ae8975df
nickmccaigntu <nick.mccaig@hotmail.co.uk>
2021-04-16 12:28:31 +0100

    fixed errors.

--- SystemsSoftwareProject/src/systemssoftwareproject/DataStructures/ClientType.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/DataStructures/ClientType.java
@@ -3,13 +3,5 @@ package systemssoftwareproject.DataStructures;
 import java.io.Serializable;
 
 public class ClientType {
-    public String ID = "TestID";
     
-    public String getID(){
-        return ID;
-    }
-    
-    protected void setID(String id){
-        this.ID = id;
-    }   
 }
