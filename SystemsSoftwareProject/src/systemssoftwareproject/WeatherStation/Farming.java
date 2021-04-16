8ce99dc4f4a86e6220885f22456867fab1695926
Ryan <rmcgouran6@gmail.com>
2021-04-16 14:12:54 +0100

    Added farming class with possible crops for each field

---
+++ SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/Farming.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package systemssoftwareproject.WeatherStation;
+
+/**
+ *
+ * @author ryan
+ */
+public class Farming {
+    public static String[] crops = {"Potato",
+                                    "Carrot",
+                                    "Wheat",
+                                    "Onion",
+                                    "Lettuce",
+                                    "Orange",
+                                    "Banana",
+                                    "Grape",
+                                    "Apple"};
+}
