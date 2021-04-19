cdc3ae4420ba18cdc33112574f39ae0b9a605c93
Hannah-Ashna <hannah.ashna.jacob@gmail.com>
2021-04-19 12:49:52 +0100

    Fixed formatting

--- SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/Farming.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/Farming.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package systemssoftwareproject.WeatherStation;
 
-/**
- *
- * @author ryan
- */
 public class Farming {
     public static String[] crops = {"Potato",
                                     "Carrot",
--- SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/Location.java
+++ SystemsSoftwareProject/src/systemssoftwareproject/WeatherStation/Location.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package systemssoftwareproject.WeatherStation;
 
 import java.util.Random;
 
-/**
- *
- * @author ryan
- */
 public class Location {
     
     public static double getGpsLat(){
